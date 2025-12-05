6693da19ad81157d5dbd8aca7558e8b380f94fd3
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2025-12-05 10:25:33 -0300

    Configurando APM

--- src/SME.NovoSGP.Abrangencia.Worker/RabbitMQMessageProcessor.cs
+++ src/SME.NovoSGP.Abrangencia.Worker/RabbitMQMessageProcessor.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Elastic.Apm;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using SME.NovoSGP.Abrangencia.Dominio.Entidades;
@@ -9,7 +11,6 @@ using SME.NovoSGP.Abrangencia.Infra.Extensions;
 using SME.NovoSGP.Abrangencia.Infra.Fila;
 using SME.NovoSGP.Abrangencia.Infra.Interfaces;
 using System.Text;
-using System.Text.Json;
 
 namespace SME.NovoSGP.Abrangencia.Worker;
 
@@ -20,6 +21,7 @@ public class RabbitMQMessageProcessor : IRabbitMQMessageProcessor
     private readonly IServicoLog _servicoLog;
     private readonly IServicoMensageria _servicoMensageria;
     private readonly ILogger<RabbitMQMessageProcessor> _logger;
+    private readonly string apmTransactionType = "WorkerRabbitAbrangencia";
 
     public RabbitMQMessageProcessor(
         IServiceScopeFactory serviceScopeFactory,
@@ -47,7 +49,8 @@ public class RabbitMQMessageProcessor : IRabbitMQMessageProcessor
             return;
         }
 
-        var transacao = _servicoTelemetria.IniciarTransacao(rota);
+        var transacao =  Agent.Tracer.StartTransaction(rota, apmTransactionType);
+        //var transacao = _servicoTelemetria.IniciarTransacao(rota);
         var mensagemRabbit = mensagem.ConverterObjectStringPraObjeto<MensagemRabbit>();
         var comandoRabbit = comandos[rota];
 
@@ -66,45 +69,52 @@ public class RabbitMQMessageProcessor : IRabbitMQMessageProcessor
                 rota);
 
             await channel.BasicAckAsync(ea.DeliveryTag, false);
+            //await servicoMensageriaMetricas.Concluido(rota);
         }
         catch (NegocioException nex)
         {
+            transacao?.CaptureException(nex);
+
             await channel.BasicAckAsync(ea.DeliveryTag, false);
-            RegistrarLog(ea, mensagemRabbit, nex, LogNivel.Negocio, $"Erros: {nex.Message}");
-            _servicoTelemetria.RegistrarExcecao(transacao, nex);
+            //await servicoMensageriaMetricas.Concluido(rota);
+
+            await RegistrarErroTratamentoMensagem(ea, mensagemRabbit, nex, LogNivel.Negocio, $"Erros: {nex.Message}");
         }
         catch (ValidacaoException vex)
         {
-            await channel.BasicAckAsync(ea.DeliveryTag, false);
-            RegistrarLog(ea, mensagemRabbit, vex, LogNivel.Negocio, $"Erros: {JsonSerializer.Serialize(vex.Mensagens())}");
-            _servicoTelemetria.RegistrarExcecao(transacao, vex);
+            transacao?.CaptureException(vex);
+
+                 channel.BasicAckAsync(ea.DeliveryTag, false);
+            //await servicoMensageriaMetricas.Concluido(rota);
+
+            await RegistrarErroTratamentoMensagem(ea, mensagemRabbit, vex, LogNivel.Negocio, $"Erros: {JsonConvert.SerializeObject(vex.Mensagens())}");
         }
         catch (Exception ex)
         {
-            _servicoTelemetria.RegistrarExcecao(transacao, ex);
-            var rejeicoes = GetRetryCount(ea.BasicProperties);
+            transacao?.CaptureException(ex);
 
+            var rejeicoes = GetRetryCount(ea.BasicProperties);
             if (++rejeicoes >= comandoRabbit.QuantidadeReprocessamentoDeadLetter)
             {
                 await channel.BasicAckAsync(ea.DeliveryTag, false);
 
-                var filaFinal = $"{ea.RoutingKey}.deadletter.final";
-
-                await _servicoMensageria.Publicar(mensagemRabbit, filaFinal,
-                    ExchangeRabbit.WorkerAbrangenciaDeadLetter,
-                    "PublicarDeadLetter");
+                var filaLimbo = $"{ea.RoutingKey}.limbo";
+                await _servicoMensageria.Publicar(mensagemRabbit, filaLimbo, ExchangeRabbit.WorkerAbrangenciaDeadLetter, "PublicarDeadLetter");
             }
-            else
-            {
-                await channel.BasicRejectAsync(ea.DeliveryTag, false);
-            }
+            else await channel.BasicRejectAsync(ea.DeliveryTag, false);
 
-            RegistrarLog(ea, mensagemRabbit, ex, LogNivel.Critico, $"Erros: {ex.Message}");
+            //await servicoMensageriaMetricas.Erro(rota);
+            await RegistrarErroTratamentoMensagem(ea, mensagemRabbit, ex, LogNivel.Critico, $"Erros: {ex.Message}");
         }
         finally
         {
-            _servicoTelemetria.FinalizarTransacao(transacao);
+            transacao?.End();
         }
+    }
+
+    protected virtual Task RegistrarErroTratamentoMensagem(BasicDeliverEventArgs ea, MensagemRabbit mensagemRabbit, Exception ex, LogNivel logNivel, string observacao)
+    {
+        return Task.CompletedTask;
     }
 
     private ulong GetRetryCount(IReadOnlyBasicProperties properties)
