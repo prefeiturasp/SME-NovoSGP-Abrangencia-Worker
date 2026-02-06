224c9833eb92a2ee65dc1dffbae475d85ae66f8e
Mario Junior <mariosj9@gmail.com>
2026-02-05 21:43:43 -0300

    AB#142909

---
+++ src/SME.NovoSGP.Abrangencia.Infra/Dtos/PerfisApiEolDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SME.NovoSGP.Abrangencia.Infra.Dtos
+{
+    public class PerfisApiEolDto
+    {
+        public string CodigoRf { get; set; }
+        public IEnumerable<Guid> Perfis { get; set; }
+        public bool PossuiCargoCJ { get; set; }
+        public bool PossuiPerfilCJ { get; set; }
+        public bool ContratoExterno { get; set; }
+    }
+}
